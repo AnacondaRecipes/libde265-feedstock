30c248a4b57165c06147e2d730a157caa4d4da28
Andrii Osipov <aosipov@anaconda.com>
2026-04-28 07:28:47 -0700

    add pthread lib

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,6 +12,13 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
   EXTRA_FLAGS="${EXTRA_FLAGS} -DDISABLE_SSE=ON"
 fi
 
+# $BUILD_PREFIX/bin/../x86_64-conda-linux-gnu/bin/ld: libde265/libde265.so.0.1.11: undefined reference to `pthread_create'
+#$BUILD_PREFIX/bin/../x86_64-conda-linux-gnu/bin/ld: libde265/libde265.so.0.1.11: undefined reference to `pthread_join'
+if [[ "$target_platform" == linux-* ]]
+then
+  export LDFLAGS="${LDFLAGS} -lpthread"
+fi
+
 cmake ${CMAKE_ARGS} -G "Ninja" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_PREFIX_PATH="$PREFIX" \
